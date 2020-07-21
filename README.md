17607806cadb0a70e697fef952705ec2b1560141
Obe Okaiwele <obokaiwele@gmail.com>
2020-07-21 15:05:52 -0400

    Trim grpc image size

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Useful collection of Dockerfile templates
 ## Size
 # Process: Start with bloated image that contains all the required functionality, then iteratively trim the image
 
-| Image | Version | Date                | Size      | Digest                                                            |
-| ----- | ----    | ------------------  | ------    | -------                                                           |
-| gRPC  | v1.20.0 | 2019.05.09 02:05:00 | 4.22 GB   | 4bdc263224c78256e3fdb8cdf84aad6055cda35a44ec0ef0aab9c0662f1d9fc5  |
+| Image | Version | Date                | Size      |
+| ----- | ----    | ------------------  | ------    |
+| gRPC  | v1.20.0 | 2019.05.09 02:05:00 | 4.22 GB   |
+| gRPC  | v1.30.2 | 2020.07.21 15:03:00 |  543 MB   |
--- grpc/build.py
+++ grpc/build.py
@@ -1,19 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+########################################################################################
+# Author: Obe Okaiwele
+# Purpose: Build gRPC (https://grpc.io/) as a Docker container
+########################################################################################
+
 import ruamel.yaml
 import requests
 import argparse
 import logging
 import sys
 import subprocess
-import json
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s %(relativeCreated)7d %(threadName)s %(processName)-10s %(levelname)-8s %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
-
 yaml = ruamel.yaml.YAML(typ='rt')
 
 
 def load(path):
-    """Load docker-compose.yml """
+    """Load YAML file"""
     with open(path, 'r') as stream:
         try:
             return yaml.load(stream)
@@ -22,7 +28,7 @@ def load(path):
             return {}
 
 def save(path, data):
-    """Save docker-compose.yml"""
+    """Save YAML file"""
     with open(path, 'w') as stream:
         try:
             yaml.dump(data, stream)
@@ -30,48 +36,45 @@ def save(path, data):
             logging.error('Failed to save yml data: ', error)
 
 
-def latest(url='http://grpc.io/release'):
-    """Get latest gRPC version"""
-    resp = requests.get(url)
-    return resp.text.strip()
-
-
 def releases(url='https://api.github.com/repos/grpc/grpc/releases'):
-    """Get Set of all gRPC releases"""
+    """Get Set of all gRPC releases. Alternatively check http://grpc.io/release for latest release"""
     resp = requests.get(url)
     data = resp.json()
-    versions = set([item['tag_name'] for item in data])
+    versions = [item['tag_name'] for item in data]
     return versions
 
 
 def set_version(path, version):
     config = load(path)
-    config['services']['ubuntu']['environment']['GRPC_VERSION'] = version
+    config['services']['grpc']['build']['args']['GRPC_VERSION'] = version
     save(path, config)
 
 
-def build(path):
-    command = 'sudo docker-compose up --build'
+def build():
+    command = 'sudo docker-compose -p grpc up --build'
     subprocess.check_call(command, shell=True)
 
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-V', '--version', help='gRPC verion to build. Defaults to latest version')
-    parser.add_argument('-c', '--config', default='docker-compose.yml', help='path to docker-compose.yml file')
+    parser.add_argument('-g', '--grpc', help='gRPC version to build. Defaults to latest version')
+    parser.add_argument('-p', '--path', default='docker-compose.yml', help='Path to docker-compose.yml')
     args = parser.parse_args()
 
-    if args.version:
-        if args.version not in releases():
-            logging.error('{} is not a valid release: '.format(args.version))
-            sys.exit(1)
+    versions = releases()
+    if args.grpc:
+        if args.grpc not in versions:
+            logging.warn('{} is not a recent release: '.format(args.grpc))
+            # sys.exit(1)
     else:
-        args.version = latest()
+        args.grpc = versions[0]
 
     # Set version as needed
-    set_version(args.config, args.version)
+    set_version(args.path, args.grpc)
 
     # Build image
-    build(args.config)
+    build()
 
- 
+
+if __name__ == '__main__':
+    main()
